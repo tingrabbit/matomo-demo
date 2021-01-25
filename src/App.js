d10eeff627875b4a64bdd17ecf8a39d0edd2c817
李玉婷-Rabbit <jak2621@dingtalk.com>
2021-01-25 11:11:24 +0800

    feat: 添加matomo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useMatomo } from "@datapunt/matomo-tracker-react";
 
-function App() {
+const MyPage = () => {
+  const { trackPageView, trackEvent } = useMatomo();
+
+  // Track page view
+  React.useEffect(() => {
+    trackPageView();
+  }, []);
+
+  const handleOnClick = () => {
+    // Track click on button
+    trackEvent({ category: "Menu", action: "Freedom" });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button type="button" onClick={handleOnClick}>
+      Click me
+    </button>
   );
-}
+};
 
-export default App;
+export default MyPage;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const instance = createInstance({
+  urlBase: "https://analytics.10bshop.com",
+  siteId: 1,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MatomoProvider value={instance}>
+      <App />
+    </MatomoProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
